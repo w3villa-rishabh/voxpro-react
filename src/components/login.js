24fe4ee70c533ce472ad853e2a03f591886850b6
Deepak <deepak.kumar@w3villa.com>
2021-03-26 16:21:29 +0530

    Set debugger

--- src/components/login.js
+++ src/components/login.js
@@ -64,6 +64,7 @@ export default function LoginComponent() {
 
   const responseGoogle = (response) => {
     console.log('Google response', response.profileObj);
+    debugger
     if (response.profileObj) {
       socialLogin(response.profileObj);
     }
