48a0c162be89a17b88a348c4a80f0aaa99ae7036
Deepak <deepak.kumar@w3villa.com>
2021-03-26 16:24:22 +0530

    fix google auto call

--- src/components/login.js
+++ src/components/login.js
@@ -65,7 +65,7 @@ export default function LoginComponent() {
   const responseGoogle = (response) => {
     console.log('Google response', response.profileObj);
     debugger
-    if (response.profileObj) {
+    if (!response.error) {
       socialLogin(response.profileObj);
     }
   };
