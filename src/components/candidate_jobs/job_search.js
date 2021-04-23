52cf542e29dda5ea4baa979de36c248c95d1cc07
w3villa-rishabh <rishabh.pandey@w3villa.com>
2021-04-23 16:19:35 +0530

    job search and job applied changes

--- src/components/candidate_jobs/job_search.js
+++ src/components/candidate_jobs/job_search.js
@@ -5,13 +5,15 @@ import {
   Card,
   InputAdornment,
   TextField,
-  Button
+  Button,
+  Switch
 } from '@material-ui/core';
 import Select from 'react-select';
 
 import WorkIcon from '@material-ui/icons/Work';
 import SearchTwoToneIcon from '@material-ui/icons/SearchTwoTone';
 import logo1 from '../../assets/images/stock-photos/c-logo.webp';
+import logo2 from '../../assets/images/stock-photos/company.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import countryList from 'react-select-country-list';
 
@@ -83,15 +85,43 @@ export default function JobSearchComponent() {
     setValue2(value2);
   };
 
+  const [state, setState] = useState({
+    checkedA: false
+  });
+
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+
   return (
     <>
       <div className="page-title">
-        <WorkIcon />
-        <div className="title">
-          <h5 className="heading pt-3">Job Search</h5>
-          {/* <p>List of all the applied jobs</p> */}
-        </div>
+        <Grid container spacing={2}>
+          <Grid item md={4} xs={4}>
+            <WorkIcon />
+            <div className="title">
+              <h5 className="heading pt-3">Job Search</h5>
+            </div>
+          </Grid>
+          <Grid item md={4} xs={4}>
+            <div className="mb-3 mt-3 ml-5">
+              <Button className="btn-neutral-info hover-scale-sm">
+                <FontAwesomeIcon icon={['fas', 'bell']} className="svg-none" />
+                <span className="px-2">Get Job Alert</span>
+              </Button>
+            </div>
+          </Grid>
+          <Grid item md={4} xs={4}>
+            <div className="mb-3 mt-3 ml-5">
+              <Button className="btn-neutral-info hover-scale-sm">
+                <FontAwesomeIcon icon={['fas', 'heart']} className="svg-none" />
+                <span className="px-2">Shortlisted Jobs</span>
+              </Button>
+            </div>
+          </Grid>
+        </Grid>
       </div>
+
       <Card className="card-box">
         <Grid container spacing={2}>
           <Grid item md={3} xs={4}>
@@ -115,7 +145,7 @@ export default function JobSearchComponent() {
               />
             </div>
           </Grid>
-          <Grid item md={2} xs={4}>
+          <Grid item md={3} xs={4}>
             <div className="mb-3 mt-3">
               <Select
                 options={options}
@@ -125,7 +155,7 @@ export default function JobSearchComponent() {
               />
             </div>
           </Grid>
-          <Grid item md={2} xs={4}>
+          <Grid item md={3} xs={4}>
             <div className="mb-3 mt-3">
               <Select
                 options={jobtype}
@@ -135,8 +165,8 @@ export default function JobSearchComponent() {
               />
             </div>
           </Grid>
-          <Grid item md={2} xs={4}>
-            <div className="mb-3 mt-3">
+          <Grid item md={3} xs={4}>
+            <div className="mb-3 mt-3 mr-3">
               <Select
                 options={jobposted}
                 value={value2}
@@ -145,17 +175,51 @@ export default function JobSearchComponent() {
               />
             </div>
           </Grid>
+        </Grid>
+        <Grid container spacing={1}>
+          <Grid item md={3} xs={4}></Grid>
           <Grid item md={2} xs={4}>
-            <div className="mb-3 mt-3 ml-5">
+            <div className="float-right mb-3">
+              <Button className="btn-neutral-info hover-scale-sm">
+                <span className="px-2">Advance Search</span>
+              </Button>
+            </div>
+          </Grid>
+          <Grid item md={2} xs={4}>
+            <div className="float-right mb-3">
               <Button className="btn-neutral-info hover-scale-sm">
                 <span className="px-2">Search</span>
+              </Button>
+            </div>
+          </Grid>
+          <Grid item md={3} xs={4}>
+            <div className="mt-2 mb-3 float-right">
+              <span className="ml-3">Relevent</span>
+              <Switch
+                onChange={handleChange}
+                checked={state.checkedA}
+                name="checkedA"
+                color="primary"
+                className="switch-medium ml-1 mr-1"
+              />
+              Recent
+            </div>
+          </Grid>
+          <Grid item md={2} xs={4}>
+            <div className="float-right mb-3">
+              <Button className="m-1 btn-transparent btn-link btn-link-primary">
+                <span>Browse Jobs </span>{' '}
+                <FontAwesomeIcon
+                  icon={['fas', 'arrow-right']}
+                  className="svg-none"
+                />
               </Button>
             </div>
           </Grid>
         </Grid>
       </Card>
 
-      <div className="title mt-3 pl-2"> 
+      <div className="title mt-3 pl-2">
         <h6 className="fh">Based on your profile and career interests</h6>
       </div>
 
@@ -174,27 +238,68 @@ export default function JobSearchComponent() {
                     />
                   </Grid>
                   <Grid item xl={6}>
-                    <div className="symbol symbol-lg-75 symbol-primary text-right">
+                    {/* <div className="symbol symbol-lg-75 symbol-primary text-right">
                       <span className="text-muted f">
                         10-shotlisted <br />
                         2-Selected
                       </span>
-                    </div>
+                    </div> */}
                   </Grid>
                 </Grid>
                 <div className="d-flex flex-column"></div>
               </div>
-              <div className="mt-5">
+              <div className="mt-3">
                 <h6 className="mb-7 fh">UX Lead and Researcher</h6>
                 <h6 className="mb-7 f">HexLab Corporation Pvt. ltd</h6>
                 <span className="f text-muted">
                   {' '}
-                  <FontAwesomeIcon icon={['fas', 'map-marker-alt']} /> Los
-                  Angeles, California, USA
+                  <FontAwesomeIcon icon={['fas', 'map-marker-alt']} /> London,
+                  UK
                 </span>
-                <p className="mt-1 f mt-0 text-muted">
-                  Compentation: €8k - €10k
-                </p>
+                <p className="mt-1 f mt-0 text-muted">Salary: £8k - £10k</p>
+              </div>
+              <Button className="btn-neutral-info hover-scale-sm mt-2">
+                <span className="px-2">
+                  {' '}
+                  <FontAwesomeIcon icon={['fas', 'heart']} />
+                </span>
+                <span> Save</span>
+              </Button>
+            </div>
+          </Grid>
+
+          <Grid item xs={12} sm={3}>
+            <div className="card card-custom gutter-b card-stretch bg-white btn rounded text-left p-4">
+              <div className="d-flex flex-column justify-content-between">
+                <Grid container spacing={4}>
+                  <Grid item xl={6}>
+                    <img
+                      style={{ height: '50px', width: '130px' }}
+                      className=""
+                      alt="..."
+                      src={logo2}
+                    />
+                  </Grid>
+                  <Grid item xl={6}>
+                    {/* <div className="symbol symbol-lg-75 symbol-primary text-right">
+                      <span className="text-muted f">
+                        10-shotlisted <br />
+                        2-Selected
+                      </span>
+                    </div> */}
+                  </Grid>
+                </Grid>
+                <div className="d-flex flex-column"></div>
+              </div>
+              <div className="mt-3">
+                <h6 className="mb-7 fh">UX Lead and Researcher</h6>
+                <h6 className="mb-7 f">HexLab Corporation Pvt. ltd</h6>
+                <span className="f text-muted">
+                  {' '}
+                  <FontAwesomeIcon icon={['fas', 'map-marker-alt']} /> London,
+                  UK
+                </span>
+                <p className="mt-1 f mt-0 text-muted">Salary: £8k - £10k</p>
               </div>
               <Button className="btn-neutral-info hover-scale-sm mt-2">
                 <span className="px-2">
@@ -219,27 +324,25 @@ export default function JobSearchComponent() {
                     />
                   </Grid>
                   <Grid item xl={6}>
-                    <div className="symbol symbol-lg-75 symbol-primary text-right">
+                    {/* <div className="symbol symbol-lg-75 symbol-primary text-right">
                       <span className="text-muted f">
                         10-shotlisted <br />
                         2-Selected
                       </span>
-                    </div>
+                    </div> */}
                   </Grid>
                 </Grid>
                 <div className="d-flex flex-column"></div>
               </div>
-              <div className="mt-5">
+              <div className="mt-3">
                 <h6 className="mb-7 fh">UX Lead and Researcher</h6>
                 <h6 className="mb-7 f">HexLab Corporation Pvt. ltd</h6>
                 <span className="f text-muted">
                   {' '}
-                  <FontAwesomeIcon icon={['fas', 'map-marker-alt']} /> Los
-                  Angeles, California, USA
+                  <FontAwesomeIcon icon={['fas', 'map-marker-alt']} />
+                  London, UK
                 </span>
-                <p className="mt-1 f mt-0 text-muted">
-                  Compentation: €8k - €10k
-                </p>
+                <p className="mt-1 f mt-0 text-muted">Salary: £8k - £10k</p>
               </div>
               <Button className="btn-neutral-info hover-scale-sm mt-2">
                 <span className="px-2">
@@ -260,76 +363,29 @@ export default function JobSearchComponent() {
                       style={{ height: '50px', width: '130px' }}
                       className=""
                       alt="..."
-                      src={logo1}
+                      src={logo2}
                     />
                   </Grid>
                   <Grid item xl={6}>
-                    <div className="symbol symbol-lg-75 symbol-primary text-right">
+                    {/* <div className="symbol symbol-lg-75 symbol-primary text-right">
                       <span className="text-muted f">
                         10-shotlisted <br />
                         2-Selected
                       </span>
-                    </div>
+                    </div> */}
                   </Grid>
                 </Grid>
                 <div className="d-flex flex-column"></div>
               </div>
-              <div className="mt-5">
+              <div className="mt-3">
                 <h6 className="mb-7 fh">UX Lead and Researcher</h6>
                 <h6 className="mb-7 f">HexLab Corporation Pvt. ltd</h6>
                 <span className="f text-muted">
                   {' '}
-                  <FontAwesomeIcon icon={['fas', 'map-marker-alt']} /> Los
-                  Angeles, California, USA
+                  <FontAwesomeIcon icon={['fas', 'map-marker-alt']} /> London,
+                  UK
                 </span>
-                <p className="mt-1 f mt-0 text-muted">
-                  Compentation: €8k - €10k
-                </p>
-              </div>
-              <Button className="btn-neutral-info hover-scale-sm mt-2">
-                <span className="px-2">
-                  {' '}
-                  <FontAwesomeIcon icon={['fas', 'heart']} />
-                </span>
-                <span> Save</span>
-              </Button>
-            </div>
-          </Grid>
-
-          <Grid item xs={12} sm={3}>
-            <div className="card card-custom gutter-b card-stretch bg-white btn rounded text-left p-4">
-              <div className="d-flex flex-column justify-content-between">
-                <Grid container spacing={4}>
-                  <Grid item xl={6}>
-                    <img
-                      style={{ height: '50px', width: '130px' }}
-                      className=""
-                      alt="..."
-                      src={logo1}
-                    />
-                  </Grid>
-                  <Grid item xl={6}>
-                    <div className="symbol symbol-lg-75 symbol-primary text-right">
-                      <span className="text-muted f">
-                        10-shotlisted <br />
-                        2-Selected
-                      </span>
-                    </div>
-                  </Grid>
-                </Grid>
-                <div className="d-flex flex-column"></div>
-              </div>
-              <div className="mt-5">
-                <h6 className="mb-7 fh">UX Lead and Researcher</h6>
-                <h6 className="mb-7 f">HexLab Corporation Pvt. ltd</h6>
-                <span className="f text-muted">
-                  {' '}
-                  <FontAwesomeIcon icon={['fas', 'map-marker-alt']} /> Los
-                  Angeles, California, USA
-                </span>
-                <p className="mt-1 f mt-0 text-muted">
-                  Compentation: €8k - €10k
-                </p>
+                <p className="mt-1 f mt-0 text-muted">Salary: £8k - £10k</p>
               </div>
               <Button className="btn-neutral-info hover-scale-sm mt-2">
                 <span className="px-2">
