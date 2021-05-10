4f0ae3b5ff86dc22aa7eb9ca9ea2ea18f49672f5
w3villa-rishabh <rishabh.pandey@w3villa.com>
2021-05-10 12:01:42 +0530

    added more questions

--- src/components/ir35_tax/new-patern.js
+++ src/components/ir35_tax/new-patern.js
@@ -591,5 +591,415 @@ export default [
         Next: '31'
       }
     ]
+  },
+  {
+    index: 24,
+    heading: 'Working arrangements',
+    question:
+      'Does your client have the right to move you from the task you originally agreed to do?',
+    p1:
+      'A worker taken on for general tasks, rather than one specific task, might be moved as and when priorities change. The client may not need the worker’s permission to move them.',
+    p2: '',
+    provious: '17',
+    status: false,
+    options: [
+      {
+        name: 'Yes',
+        value: 'a',
+        aria_label: 'A',
+        Next: '32'
+      },
+      {
+        name: 'No, you would have to agree',
+        value: 'b',
+        aria_label: 'B',
+        Next: '32'
+      },
+      {
+        name:
+          'No, that would require a new contract or formal working arrangement',
+        value: 'c',
+        aria_label: 'C',
+        Next: '32'
+      }
+    ]
+  },
+  {
+    index: 25,
+    heading: 'Working arrangements',
+    question:
+      'Does your client have the right to move you from the task you originally agreed to do?',
+    p1:
+      'A worker taken on for general tasks, rather than one specific task, might be moved as and when priorities change. The client may not need the worker’s permission to move them.',
+    p2: '',
+    provious: '18',
+    status: false,
+    options: [
+      {
+        name: 'Yes',
+        value: 'a',
+        aria_label: 'A',
+        Next: '33'
+      },
+      {
+        name: 'No, you would have to agree',
+        value: 'b',
+        aria_label: 'B',
+        Next: '33'
+      },
+      {
+        name:
+          'No, that would require a new contract or formal working arrangement',
+        value: 'c',
+        aria_label: 'C',
+        Next: '33'
+      }
+    ]
+  },
+  {
+    index: 26,
+    heading: 'Substitutes and helpers',
+    question:
+      'Have you paid another person to do a significant amount of this work?',
+    p1: '',
+    p2: '',
+    provious: '18',
+    status: false,
+    options: [
+      {
+        name: 'Yes',
+        value: 'a',
+        aria_label: 'A',
+        Next: '34'
+      },
+      {
+        name: 'No',
+        value: 'b',
+        aria_label: 'B',
+        Next: '34'
+      }
+    ]
+  },
+  {
+    index: 27,
+    heading: 'Working arrangements',
+    question: 'Does your client have the right to decide your working hours?',
+    p1: '',
+    p2: '',
+    provious: '19',
+    status: false,
+    options: [
+      {
+        name: 'Yes',
+        value: 'a',
+        aria_label: 'A',
+        Next: '35'
+      },
+      {
+        name: 'No, you solely decide',
+        value: 'b',
+        aria_label: 'B',
+        Next: '35'
+      },
+      {
+        name: 'No, you and your client agree',
+        value: 'c',
+        aria_label: 'C',
+        Next: '35'
+      },
+      {
+        name: 'No, the work is based on agreed deadlines',
+        value: 'd',
+        aria_label: 'D',
+        Next: '35'
+      }
+    ]
+  },
+  {
+    index: 28,
+    heading: 'Working arrangements',
+    question: 'Does your client have the right to decide how the work is done?',
+    p1:
+      'This can include your client instructing, guiding or advising the way the task should be completed.',
+    p2:
+      'This is not relevant if it is highly skilled work. For example, an airline pilot.',
+    provious: '20',
+    status: false,
+    options: [
+      {
+        name: 'Yes',
+        value: 'a',
+        aria_label: 'A',
+        Next: '36'
+      },
+      {
+        name: 'No, you solely decideo',
+        value: 'b',
+        aria_label: 'B',
+        Next: '36'
+      },
+      {
+        name: 'No, you and your client agree together',
+        value: 'c',
+        aria_label: 'C',
+        Next: '36'
+      },
+      {
+        name: 'Not relevant, it is highly skilled work',
+        value: 'd',
+        aria_label: 'D',
+        Next: '36'
+      }
+    ]
+  },
+  {
+    index: 29,
+    heading: 'Working arrangements',
+    question: 'Does your client have the right to decide your working hours?',
+    p1: '',
+    p2: '',
+    provious: '21',
+    status: false,
+    options: [
+      {
+        name: 'Yes',
+        value: 'a',
+        aria_label: 'A',
+        Next: '37'
+      },
+      {
+        name: 'No, you solely decide',
+        value: 'b',
+        aria_label: 'B',
+        Next: '37'
+      },
+      {
+        name: 'No, you and your client agree',
+        value: 'c',
+        aria_label: 'C',
+        Next: '37'
+      },
+      {
+        name: 'No, the work is based on agreed deadlines',
+        value: 'd',
+        aria_label: 'D',
+        Next: '37'
+      }
+    ]
+  },
+  {
+    index: 30,
+    heading: 'Working arrangements',
+    question: 'Does your client have the right to decide how the work is done?',
+    p1:
+      'This can include your client instructing, guiding or advising the way the task should be completed.',
+    p2:
+      'This is not relevant if it is highly skilled work. For example, an airline pilot.',
+    provious: '22',
+    status: false,
+    options: [
+      {
+        name: 'Yes',
+        value: 'a',
+        aria_label: 'A',
+        Next: '38'
+      },
+      {
+        name: 'No, you solely decideo',
+        value: 'b',
+        aria_label: 'B',
+        Next: '38'
+      },
+      {
+        name: 'No, you and your client agree together',
+        value: 'c',
+        aria_label: 'C',
+        Next: '38'
+      },
+      {
+        name: 'Not relevant, it is highly skilled work',
+        value: 'd',
+        aria_label: 'D',
+        Next: '38'
+      }
+    ]
+  },
+  {
+    index: 31,
+    heading: 'Working arrangements',
+    question: 'Does your client have the right to decide your working hours?',
+    p1: '',
+    p2: '',
+    provious: '23',
+    status: false,
+    options: [
+      {
+        name: 'Yes',
+        value: 'a',
+        aria_label: 'A',
+        Next: '39'
+      },
+      {
+        name: 'No, you solely decide',
+        value: 'b',
+        aria_label: 'B',
+        Next: '39'
+      },
+      {
+        name: 'No, you and your client agree',
+        value: 'c',
+        aria_label: 'C',
+        Next: '39'
+      },
+      {
+        name: 'No, the work is based on agreed deadlines',
+        value: 'd',
+        aria_label: 'D',
+        Next: '39'
+      }
+    ]
+  },
+  {
+    index: 32,
+    heading: 'Working arrangements',
+    question: 'Does your client have the right to decide how the work is done?',
+    p1:
+      'This can include your client instructing, guiding or advising the way the task should be completed.',
+    p2:
+      'This is not relevant if it is highly skilled work. For example, an airline pilot.',
+    provious: '24',
+    status: false,
+    options: [
+      {
+        name: 'Yes',
+        value: 'a',
+        aria_label: 'A',
+        Next: '40'
+      },
+      {
+        name: 'No, you solely decideo',
+        value: 'b',
+        aria_label: 'B',
+        Next: '40'
+      },
+      {
+        name: 'No, you and your client agree together',
+        value: 'c',
+        aria_label: 'C',
+        Next: '40'
+      },
+      {
+        name: 'Not relevant, it is highly skilled work',
+        value: 'd',
+        aria_label: 'D',
+        Next: '40'
+      }
+    ]
+  },
+  {
+    index: 33,
+    heading: 'Working arrangements',
+    question: 'Does your client have the right to decide how the work is done?',
+    p1:
+      'This can include your client instructing, guiding or advising the way the task should be completed.',
+    p2:
+      'This is not relevant if it is highly skilled work. For example, an airline pilot.',
+    provious: '25',
+    status: false,
+    options: [
+      {
+        name: 'Yes',
+        value: 'a',
+        aria_label: 'A',
+        Next: '41'
+      },
+      {
+        name: 'No, you solely decideo',
+        value: 'b',
+        aria_label: 'B',
+        Next: '41'
+      },
+      {
+        name: 'No, you and your client agree together',
+        value: 'c',
+        aria_label: 'C',
+        Next: '41'
+      },
+      {
+        name: 'Not relevant, it is highly skilled work',
+        value: 'd',
+        aria_label: 'D',
+        Next: '41'
+      }
+    ]
+  },
+  {
+    index: 34,
+    heading: 'Working arrangements',
+    question:
+      'Does your client have the right to move you from the task you originally agreed to do?',
+    p1:
+      'A worker taken on for general tasks, rather than one specific task, might be moved as and when priorities change. The client may not need the worker’s permission to move them.',
+    p2: '',
+    provious: '26',
+    status: false,
+    options: [
+      {
+        name: 'Yes',
+        value: 'a',
+        aria_label: 'A',
+        Next: '42'
+      },
+      {
+        name: 'No, you would have to agree',
+        value: 'b',
+        aria_label: 'B',
+        Next: '42'
+      },
+      {
+        name:
+          'No, that would require a new contract or formal working arrangement',
+        value: 'c',
+        aria_label: 'C',
+        Next: '42'
+      }
+    ]
+  },
+  {
+    index: 35,
+    heading: 'Working arrangements',
+    question:
+      'Does your client have the right to decide where you do the work?',
+    p1: '',
+    p2: '',
+    provious: '27',
+    status: false,
+    options: [
+      {
+        name: 'Yes',
+        value: 'a',
+        aria_label: 'A',
+        Next: '43'
+      },
+      {
+        name: 'No, you solely decide',
+        value: 'b',
+        aria_label: 'B',
+        Next: '43'
+      },
+      {
+        name: 'No, the task sets the location',
+        value: 'c',
+        aria_label: 'C',
+        Next: '43'
+      },
+      {
+        name:
+          'No, some work has to be done in an agreed location and some can be your choice',
+        value: 'd',
+        aria_label: 'D',
+        Next: '43'
+      }
+    ]
   }
 ];
