0537e7dfc966a0f6e7c74a8be22a618cade125c8
Deepak <deepak.kumar@w3villa.com>
2021-05-21 13:40:58 +0530

    document bottom card change

--- src/components/documents/onboarding_documents.js
+++ src/components/documents/onboarding_documents.js
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
 
-import { Card, Button, Grid, CardContent } from '@material-ui/core';
+import { Card, Button, Grid } from '@material-ui/core';
 import PostAddIcon from '@material-ui/icons/PostAdd';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useHistory } from 'react-router-dom';
@@ -148,10 +148,7 @@ export default function OnBoardDocument() {
         <Grid item md={3} xs={12}>
           <Card className="card-box p-3 h-100">
             <div className="font-12 font-size-sm text-uppercase text-second mt-2">
-              {currentUser.role === 'candidate' && 'Requests for information'}
-              {(currentUser.role === 'agency' ||
-                currentUser.role === 'company') &&
-                'Candidate documents due to expire'}
+              Pending requests for information
             </div>
             <div className="d-flex py-2 align-items-center">
               <div className="d-50 rounded border-0 card-icon-wrapper flex-shrink-0 bg-first text-white btn-icon text-center shadow-first mr-3">
@@ -172,11 +169,7 @@ export default function OnBoardDocument() {
         <Grid item md={3} xs={12}>
           <Card className="card-box p-3 h-100">
             <div className="font-12 font-size-sm text-uppercase text-second mt-2">
-              {currentUser.role === 'candidate' &&
-                'Remaining Documents to upload'}
-              {(currentUser.role === 'agency' ||
-                currentUser.role === 'company') &&
-                'Placements documents due to expire'}
+              Total request for information
             </div>
             <div className="d-flex py-2 align-items-center">
               <div className="d-50 rounded border-0 card-icon-wrapper flex-shrink-0 bg-warning text-white btn-icon text-center shadow-warning mr-3">
@@ -197,11 +190,7 @@ export default function OnBoardDocument() {
         <Grid item md={3} xs={12}>
           <Card className="card-box p-3 h-100">
             <div className="font-12 font-size-sm text-uppercase text-second mt-2">
-              {currentUser.role === 'candidate' && 'Document due to expire'}
-              {currentUser.role === 'agency' &&
-                'Client documents due to expire'}
-              {currentUser.role === 'company' &&
-                'Agency documents due to expire'}
+              Documents
             </div>
             <div className="d-flex py-2 align-items-center">
               <div className="d-50 rounded border-0 card-icon-wrapper flex-shrink-0 bg-danger text-white btn-icon text-center mr-3 shadow-danger">
@@ -220,21 +209,26 @@ export default function OnBoardDocument() {
           </Card>
         </Grid>
         <Grid item md={3} xs={12}>
-          <CardContent className="bg-brand-facebook h-100 text-white">
-            <div className="font-12 font-size-sm text-uppercase text-white mt-2">
-              Tasks Overview
+          <Card className="h-100 card-box p-3 bg-heavy-rain">
+            <div className="font-12 font-size-sm text-uppercase text-second mt-2">
+              Overview
             </div>
-            <div className="d-flex py-2 align-items-center text-white">
-              <div className="d-50 rounded border-0 card-icon-wrapper flex-shrink-0 bg-success text-white btn-icon text-center shadow-success mr-3">
+            <div className="d-flex py-2 align-items-center">
+              {/* <div className="d-50 rounded border-0 card-icon-wrapper flex-shrink-0 bg-success btn-icon text-center shadow-success mr-3">
                 <FontAwesomeIcon
                   icon={['fas', 'tachometer-alt']}
                   className="display-4"
                 />
-              </div>
-              <div className="ml-1 text-white">
-                2 tasks
+              </div> */}
+              <div className="ml-1">
+                <span>2 Documents uploaded</span>
                 <br />
-                <div className="mb-2 font-style text-white">due today</div>{' '}
+                <span>3 Documents due to expire</span>
+                <br />
+                <span>2 Total request for information</span>
+                <br />
+                <span>2 Pending request for information</span>
+                <br />
               </div>
             </div>
 
@@ -247,7 +241,7 @@ export default function OnBoardDocument() {
               </a>{' '}
               that accepted your invitation to connect.
             </div> */}
-          </CardContent>
+          </Card>
         </Grid>
       </Grid>
 
