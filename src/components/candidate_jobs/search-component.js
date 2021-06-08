f1a3c1ba61d5af6ec07a6b5814b0dddc6a4e5746
Deepak <deepak.kumar@w3villa.com>
2021-06-08 16:32:17 +0530

    remove duplicate

--- src/components/candidate_jobs/search-component.js
+++ src/components/candidate_jobs/search-component.js
@@ -17,6 +17,7 @@ import { ClipLoader } from 'react-spinners';
 import axios from 'axios';
 import { connect } from 'react-redux';
 import { setSearchResult, callSearch } from '../../reducers/ThemeOptions';
+import _ from 'lodash';
 
 import { useHistory } from 'react-router';
 import CountryCity from '../../assets/city-country';
@@ -95,8 +96,8 @@ const JobSearchComponent = (props) => {
           if (response.data.success) {
             console.log('response.data', response.data.jobs);
             let searchResult = props.searchResult.concat(response.data.jobs);
-            props.setSearchResult(searchResult, response.data.page_info);
-            // setPageNo(response.data.page_info);
+            let uniqueSearch = _.unionBy(searchResult, 'id');
+            props.setSearchResult(uniqueSearch, response.data.page_info);
             props.searchFilter.page = response.data.page_info.current_page;
           } else {
             props.setSearchResult([]);
